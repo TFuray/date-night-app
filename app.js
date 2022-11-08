15eaa1b2c20c9122e192725c83830450a1cd97c7
TFuray <pk.master881@gmail.com>
2022-11-08 14:14:06 -0800

    added file structure and connected mongodb

--- app.js
+++ app.js
@@ -2,3 +2,26 @@ const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
+const port = process.env.PORT 
+
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'Date-Ideas'
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true})
+    .then(client => {
+        console.log(`connected to ${dbName} database`)
+        db = client.db(dbName)
+    })
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+
+
+app.listen(process.env.PORT || port, ()=>{
+    console.log(`Server running on port ${port}`)
+})

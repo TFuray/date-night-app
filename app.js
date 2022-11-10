32c39c083a168d7a799048198bf6b4b2ff39ab60
TFuray <pk.master881@gmail.com>
2022-11-09 17:06:30 -0800

    updated main page and added routes

--- app.js
+++ app.js
@@ -1,27 +1,50 @@
 const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
-require('dotenv').config({path: './config.env'})
-const port = process.env.PORT 
+require('dotenv').config({ path: './config.env' })
+const port = process.env.PORT
 
-let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'Date-Ideas'
-console.log(dbConnectionStr)
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true})
-    .then(client => {
-        console.log(`connected to ${dbName} database`)
-        db = client.db(dbName)
-    })
+let db, ideasCollection
+const dbConnectionStr = process.env.DB_STRING,
+      dbName = 'Date-Ideas'
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }).then(
+  client => {
+    console.log(`connected to ${dbName} database`)
+    db = client.db(dbName)
+    ideasCollection = db.collection('ideas')
+  }
+)
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+app.get('/', (request, response) => {
+  try {
+    db.collection('ideas')
+      .find()
+      .toArray()
+      .then(results => {
+        console.log(results)
+        res.render('index.ejs', { ideas: results })
+      })
+  } catch (error) {
+    console.error(error)
+  }
+})
 
+app.post('/addIdea', (request, response) => {
+  ideasCollection
+    .insertOne(request.body)
+    .then(result => {
+      console.log(`successful post of ${result}`)
+      res.redirect('/')
+    })
+    .catch(error => console.error(error))
+})
 
-
-app.listen(process.env.PORT || port, ()=>{
-    console.log(`Server running on port ${port}`)
+app.listen(process.env.PORT || port, () => {
+  console.log(`Server running on port ${port}`)
 })
